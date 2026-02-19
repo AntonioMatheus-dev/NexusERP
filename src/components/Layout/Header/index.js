9aed1b82e2e28fe85bfe398b96a1eb7637950c83
AntonioMatheus-dev <matheus69969@gmail.com>
2026-02-19 10:30:53 -0300

    feat: modernize header with react-icons and pointer-events fixes

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { FaBell, FaUserCircle, FaGlobe, FaSignOutAlt } from 'react-icons/fa';
 import './Header.css';
 
 const Header = ({ onLogout }) => {
   return (
     <header className="dashboard-header">
       <div className="header-left">
-        <img src="/image/NexusERP_1_-removebg-preview 2.png" alt="Nexus Logo" className="header-logo" />
+        <img src="/image/NexusERP_logo.png" alt="Nexus Logo" className="header-logo" />
       </div>
       <div className="header-right">
-        <button className="icon-btn">🔔</button>
+        <button className="icon-btn" title="Notificações">
+          <FaBell size={18} />
+        </button>
         <div className="user-profile">
-          <span className="user-icon">👤</span>
+          <FaUserCircle size={26} className="user-icon" />
           <span className="user-name">User</span>
         </div>
-        <button className="icon-btn">🌐</button>
-        <button className="btn-logout-header" onClick={onLogout} title="Sair">🚪</button>
+        <button className="icon-btn" title="Idioma">
+          <FaGlobe size={18} />
+        </button>
+        <button className="btn-logout-header" onClick={onLogout} title="Sair">
+          <FaSignOutAlt size={18} />
+        </button>
       </div>
     </header>
   );
